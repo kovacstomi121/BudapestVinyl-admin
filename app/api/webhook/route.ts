6d3cbbfcc827e50cc04aeecfc760fa1cc949f9f8
kovacstomi121 <97965239+kovacstomi121@users.noreply.github.com>
2023-11-16 19:04:19 +0100

    Update route.ts

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,7 +3,7 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 import { stripe } from "@/lib/stripe";
-import prismadb from "@/lib/prismadb";
+import { prismadb } from "@/lib/prismadb";
 
 export async function POST(req: Request) {
   // Kérés szövegének beolvasása

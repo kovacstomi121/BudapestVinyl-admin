13c3257cb7abbeeb2373c6c90e1d0ac6b548db07
kovacstomi121 <97965239+kovacstomi121@users.noreply.github.com>
2023-11-17 16:56:05 +0100

    Update route.ts

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,33 +1,17 @@
-import { NextResponse } from "next/server";
-import { prismadb } from "@/lib/prismadb";
-import { z } from "zod";
-
-const FormSchema = z.object({
-  email: z.string().min(1, "Email megadása kötelező").email("Helytelen email"),
-  password: z
-    .string()
-    .min(1, "Add meg a jelszót")
-    .min(8, "Legalább 8 karakternek kell lennie"),
+const store = await prismadb.store.findFirst({
+  where: {
+    user: {
+      email: values.email,
+    },
+  },
+  include: {
+    user: true, // Ez azért kell, hogy a kapcsolódó felhasználó objektum is elkészüljön
+  },
 });
-export async function POST(req: Request, values: z.infer<typeof FormSchema>) {
-  try {
-    // Felhasználóhoz tartozó bolt keresése a megadott adatok alapján
-    const store = await prismadb.store.findFirst({
-      where: {
-        user: {
-          email: values.email,
-        },
-      },
-    });
 
-    if (!store) {
-      // Ha nincs bolt, irányítás a root oldalra
-      return NextResponse.redirect("/root");
-    } else {
-      return NextResponse.json(store);
-    }
-  } catch (error) {
-    console.error("[STORES_POST]", error);
-    return new NextResponse("Belső hiba", { status: 500 });
-  }
+if (store) {
+  return NextResponse.json(store);
+} else {
+  // Ha nincs bolt, irányítás a root oldalra
+  return NextResponse.redirect("/root");
 }

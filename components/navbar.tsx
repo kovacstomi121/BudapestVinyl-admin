9303bc5e37e62044e426c896b6dc57aee633d15b
kovacstomi121 <97965239+kovacstomi121@users.noreply.github.com>
2023-11-20 13:57:46 +0100

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,32 +1,80 @@
-import { useSession, signOut, getSession } from "next-auth/react";
-import { redirect } from "next/navigation";
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { cn } from "@/lib/utils";
 import StoreSwitcher from "@/components/store-switcher";
 import { MainNav } from "@/components/main-nav";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { prismadb } from "@/lib/prismadb";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import UserAccountNav from "./UserAccountNav";
-import { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Store } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const Navbar = async () => {
-  const session = await getServerSession();
+const Navbar = () => {
+  const router = useRouter();
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [stores, setStores] = useState<Store[]>([]);
 
-  if (!session) {
-    redirect("/sign-in");
-  }
+  useEffect(() => {
+    const fetchStores = async () => {
+      const session = await getServerSession();
 
-  const userId = session.user.id;
+      if (!session) {
+        router.push("/sign-in");
+        return null;
+      }
 
-  const stores = await prismadb.store.findMany({
-    where: {
-      userId,
-    },
-  });
+      const userId = session.user.id;
+
+      const storesData = await prismadb.store.findMany({
+        where: {
+          userId,
+        },
+      });
+
+      setStores(storesData);
+    };
+
+    fetchStores();
+  }, [router]);
+
+  const handleMobileToggle = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
+  const handleLinkClick = () => {
+    setMobileMenuOpen(false);
+  };
+
+  const renderRoutes = (stores: Store[]) => {
+    return stores.map((store) => (
+      <Link
+        key={store.id}
+        href={`/${store.id}`}
+        className={cn(
+          "text-sm font-medium transition-colors",
+          router.pathname === `/${store.id}`
+            ? "text-black dark:text-white"
+            : "text-muted-foreground"
+        )}
+        onClick={handleLinkClick}
+      >
+        {store.name}
+      </Link>
+    ));
+  };
+
+  const renderMobileMenu = (stores: Store[]) => {
+    return (
+      <div className="lg:hidden flex flex-col items-center space-y-2 mt-4">
+        {renderRoutes(stores)}
+        <UserAccountNav />
+      </div>
+    );
+  };
 
   return (
     <div className="border-b">
@@ -35,7 +83,13 @@ const Navbar = async () => {
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <ThemeToggle />
-          {session?.user ? <UserAccountNav /> : null}
+          <button
+            className="lg:hidden text-white p-2"
+            onClick={handleMobileToggle}
+          >
+            ☰
+          </button>
+          {mobileMenuOpen && renderMobileMenu(stores)}
         </div>
       </div>
     </div>

cc24ed9300fe78055773018fe824230995262951
kovacstomi121 <97965239+kovacstomi121@users.noreply.github.com>
2023-11-21 11:26:38 +0100

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,19 @@
-"use client";
-import React, { useState } from "react";
-import { Navbar } from "@material-tailwind/react";
-import { Bars3Icon } from "@heroicons/react/24/outline";
-import { useSession } from "next-auth/react";
+	import { useSession, signOut, getSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 import StoreSwitcher from "@/components/store-switcher";
 import { MainNav } from "@/components/main-nav";
 import { ThemeToggle } from "@/components/theme-toggle";
-import UserAccountNav from "./UserAccountNav";
+import { prismadb } from "@/lib/prismadb";
+import { useState } from "react";
 import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-import prismadb from "@/lib/prismadb";
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import UserAccountNav from "./UserAccountNav";
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
 
-export async function NavbarDefault() {
-  const [toggleMenu, setToggleMenu] = useState(false);
+const prisma = new PrismaClient();
+
+const Navbar = async () => {
   const session = await getServerSession();
 
   if (!session) {
@@ -28,34 +29,17 @@ export async function NavbarDefault() {
   });
 
   return (
-    <Navbar className="border-b mx-auto max-w-screen-xl px-4 py-2 lg:px-8 lg:py-4">
-      <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <ThemeToggle />
           {session?.user ? <UserAccountNav /> : null}
-          {/* Mobile navigation toggle */}
-          <div className="lg:hidden flex items-center">
-            <button onClick={() => setToggleMenu(!toggleMenu)}>
-              <Bars3Icon className="h-6" />
-            </button>
-          </div>
         </div>
       </div>
-      {/* mobile navigation */}
-      <div
-        className={`fixed z-40 w-full bg-gray-100 overflow-hidden flex flex-col lg:hidden gap-12 origin-top duration-700 ${
-          !toggleMenu ? "h-0" : "h-full"
-        }`}
-      >
-        <div className="px-8">
-          <div className="flex flex-col gap-8 font-bold tracking-wider">
-            <MainNav className="mx-6" />
-            {session?.user ? <UserAccountNav /> : null}
-          </div>
-        </div>
-      </div>
-    </Navbar>
+    </div>
   );
-}
+};
+
+export default Navbar;

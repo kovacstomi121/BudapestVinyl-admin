6112a2f34fb6ed7ac693a667c4218ff73f6d3ac8
kovacstomi121 <97965239+kovacstomi121@users.noreply.github.com>
2023-11-21 11:27:02 +0100

    Update layout.tsx

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,5 +1,5 @@
 import { redirect, useRouter } from "next/navigation";
-import { NavbarDefault } from "@/components/navbar";
+import { Navbar } from "@/components/navbar";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import { prismadb } from "@/lib/prismadb";
@@ -30,7 +30,7 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <NavbarDefault />
+      <Navbar/>
       {children}
     </>
   );

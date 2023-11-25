9b273e1c6995f54cbca6fcedaf4a5d01ebbc7070
kovacstomi121 <97965239+kovacstomi121@users.noreply.github.com>
2023-11-25 23:44:21 +0100

    Update route.ts

--- app/api/[storeId]/search/route.ts
+++ app/api/[storeId]/search/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/options";
+
 import { prismadb } from "@/lib/prismadb";
 
 const corsHeaders = {
